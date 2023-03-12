e34b9df4a7b8895dfa10a7840e716895a4171d91
Zix-Dev <Zix.Developmen@gmail.com>
2023-03-12 18:23:17 +0100

    Imported Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public t = ['', ''];
-  public col: { a: string; b: string }[] = [];
-  title = 'my-schedule';
-  public constructor(public db: DatabaseService) {
-    this.db.get<{ a: string; b: string }>('test').subscribe((a) => (this.col = a))
-  }
-  public test() {
-    this.db.put({ a: this.t[0], b: this.t[1] }, 'test')
-  }
-  public l(a: any): any {
-    console.log(a);
-    return a;
-  }
 }
---
+++ src/app/modules/externals/angular-material/angular-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, MatDatepickerModule
+  ],
+  exports: [
+    MatDatepickerModule
+  ]
+})
+export class AngularMaterialModule { }
