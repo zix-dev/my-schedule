a201f8e39c869e2ce404fe0c7048cc0b4c19daaf
Zix-Dev <Zix.Developmen@gmail.com>
2023-03-18 12:23:08 +0100

    small changes

--- src/app/modules/configuration/components/personal-rooms-and-machines/personal-rooms-and-machines.component.ts
+++ src/app/modules/configuration/components/personal-rooms-and-machines/personal-rooms-and-machines.component.ts
@@ -1,3 +1,4 @@
+import { Employee, Room, Machine } from './../../models/configurations.models';
 import { ConfigService } from './../../services/config.service';
 import { Component } from '@angular/core';
 
@@ -8,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class PersonalRoomsAndMachinesComponent {
 
+  public sidebarOpened: boolean = false;
+
+  public selectedEmployee?: Employee;
+
+  public selectedRoom?: Room;
+
+  public selectedMachine?: Machine;
+
   public constructor(public config: ConfigService) { }
+
+  public selectEmployee(e: Employee): void {
+    this.selectedEmployee = e;
+    this.selectedRoom = undefined;
+    this.selectedMachine = undefined;
+    this.sidebarOpened = true;
+  }
+
+  public selectRoom(r: Room): void {
+    this.selectedEmployee = undefined;
+    this.selectedRoom = r;
+    this.selectedMachine = undefined;
+    this.sidebarOpened = true;
+  }
+
+  public selectMachine(m: Machine): void {
+    this.selectedEmployee = undefined;
+    this.selectedRoom = undefined;
+    this.selectedMachine = m;
+    this.sidebarOpened = true;
+  }
 
 }
